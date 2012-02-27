6bd8b485615b7c8252f5b71b391deda5e4704bc9
Alex Sexton <AlexSexton@gmail.com>
2012-02-27 05:52:49 -0600

    Better test descriptions.

--- test/tests.js
+++ test/tests.js
@@ -402,154 +402,157 @@ describe( "MessageFormat", function () {
 
   describe( "Message Formatting", function () {
 
-    it("has a precompile function", function () {
-      var mf = new MessageFormat( 'en' );
-      expect(mf.precompile).to.be.a('function');
-    });
-
-    it("precompiles to a string", function () {
-      var mf = new MessageFormat( 'en' );
-
-      expect(mf.precompile(mf.parse("test"))).to.be.a('string');
-    });
-
-    it("has a compile function", function () {
-      var mf = new MessageFormat( 'en' );
-      expect(mf.compile).to.be.a('function');
-    });
-
-    it("compiles to a function", function () {
-      var mf = new MessageFormat( 'en' );
-
-      expect(mf.compile("test")).to.be.a('function');
-    });
-
-    it("can output a non-formatted string", function () {
-      var mf = new MessageFormat( 'en' );
-
-      expect((mf.compile("This is a string."))()).to.eql("This is a string.");
-    });
-
-    it("can substitute named variables", function () {
-      var mf = new MessageFormat( 'en' );
-
-      expect((mf.compile("The var is {VAR}."))({"VAR":5})).to.eql("The var is 5.");
-    });
-
-    it("can substitute shorthand variables", function () {
-      var mf = new MessageFormat( 'en' );
-
-      expect((mf.compile("{VAR, select, other{The var is #.}}"))({"VAR":5})).to.eql("The var is 5.");
-    });
-
-    it("obeys plural functions", function () {
-      var mf = new MessageFormat( 'fake', function ( x ) {
-        return 'few';
+    describe( "Basic API", function () {
+      it("has a precompile function", function () {
+        var mf = new MessageFormat( 'en' );
+        expect(mf.precompile).to.be.a('function');
       });
 
-      expect((mf.compile("res: {val, plural, few{wasfew} other{failed}}"))({val:0})).to.be( "res: wasfew" );
-      expect((mf.compile("res: {val, plural, few{wasfew} other{failed}}"))({val:1})).to.be( "res: wasfew" );
-      expect((mf.compile("res: {val, plural, few{wasfew} other{failed}}"))({val:2})).to.be( "res: wasfew" );
-      expect((mf.compile("res: {val, plural, few{wasfew} other{failed}}"))({val:3})).to.be( "res: wasfew" );
-      expect((mf.compile("res: {val, plural, few{wasfew} other{failed}}"))({})).to.be( "res: wasfew" );
+      it("precompiles to a string", function () {
+        var mf = new MessageFormat( 'en' );
+
+        expect(mf.precompile(mf.parse("test"))).to.be.a('string');
+      });
+
+      it("has a compile function", function () {
+        var mf = new MessageFormat( 'en' );
+        expect(mf.compile).to.be.a('function');
+      });
+
+      it("compiles to a function", function () {
+        var mf = new MessageFormat( 'en' );
+
+        expect(mf.compile("test")).to.be.a('function');
+      });
+
+      it("can output a non-formatted string", function () {
+        var mf = new MessageFormat( 'en' );
+
+        expect((mf.compile("This is a string."))()).to.eql("This is a string.");
+      });
+
+      it("can substitute named variables", function () {
+        var mf = new MessageFormat( 'en' );
+
+        expect((mf.compile("The var is {VAR}."))({"VAR":5})).to.eql("The var is 5.");
+      });
+
+      it("can substitute shorthand variables", function () {
+        var mf = new MessageFormat( 'en' );
+
+        expect((mf.compile("{VAR, select, other{The var is #.}}"))({"VAR":5})).to.eql("The var is 5.");
+      });
+
+      it("obeys plural functions", function () {
+        var mf = new MessageFormat( 'fake', function ( x ) {
+          return 'few';
+        });
+
+        expect((mf.compile("res: {val, plural, few{wasfew} other{failed}}"))({val:0})).to.be( "res: wasfew" );
+        expect((mf.compile("res: {val, plural, few{wasfew} other{failed}}"))({val:1})).to.be( "res: wasfew" );
+        expect((mf.compile("res: {val, plural, few{wasfew} other{failed}}"))({val:2})).to.be( "res: wasfew" );
+        expect((mf.compile("res: {val, plural, few{wasfew} other{failed}}"))({val:3})).to.be( "res: wasfew" );
+        expect((mf.compile("res: {val, plural, few{wasfew} other{failed}}"))({})).to.be( "res: wasfew" );
+      });
+
+      it("throws an error when no `other` option is found - plurals", function () {
+        var mf = new MessageFormat( 'en' );
+        expect(function(){ var x = mf.compile("{X, plural, someoption{a}}"); }).to.throwError();
+      });
+
+      it("throws an error when no `other` option is found - selects", function () {
+        var mf = new MessageFormat( 'en' );
+        expect(function(){ var x = mf.compile("{X, select, someoption{a}}"); }).to.throwError();
+      });
     });
 
-    it("throws an error when no `other` option is found - plurals", function () {
-      var mf = new MessageFormat( 'en' );
-      expect(function(){ var x = mf.compile("{X, plural, someoption{a}}"); }).to.throwError();
+    describe("Real World Uses", function  () {
+      it("can parse complex, real-world messages with nested selects and plurals with offsets", function () {
+        var input = "" +
+        "{PERSON} added {PLURAL_NUM_PEOPLE, plural, offset:1" +
+        "     =0 {no one}"+
+        "     =1 {just {GENDER, select, male {him} female {her} other{them}}self}"+
+        "    one {{GENDER, select, male {him} female {her} other{them}}self and one other person}"+
+        "  other {{GENDER, select, male {him} female {her} other{them}}self and # other people}"+
+        "} to {GENDER, select,"+
+        "   male {his}"+
+        " female {her}"+
+        "  other {their}"+
+        "} group.";
+
+        var mf = new MessageFormat( 'en' );
+        var mf_func = mf.compile( input );
+
+        expect( mf_func({
+            PLURAL_NUM_PEOPLE : 0,
+            PERSON : "Allie Sexton",
+            GENDER: "female"
+        }) ).to.eql('Allie Sexton added no one to her group.');
+
+        expect( mf_func({
+            PLURAL_NUM_PEOPLE : 1,
+            PERSON : "Allie Sexton",
+            GENDER: "female"
+        }) ).to.eql('Allie Sexton added just herself to her group.');
+
+        expect( mf_func({
+            PLURAL_NUM_PEOPLE : 2,
+            PERSON : "Allie Sexton",
+            GENDER: "female"
+        }) ).to.eql('Allie Sexton added herself and one other person to her group.');
+
+        expect( mf_func({
+            PLURAL_NUM_PEOPLE : 3,
+            PERSON : "Allie Sexton",
+            GENDER: "female"
+        }) ).to.eql('Allie Sexton added herself and 2 other people to her group.');
+
+        expect( mf_func({
+            PLURAL_NUM_PEOPLE : 0,
+            PERSON : "Alex Sexton",
+            GENDER: "male"
+        }) ).to.eql('Alex Sexton added no one to his group.');
+
+        expect( mf_func({
+            PLURAL_NUM_PEOPLE : 1,
+            PERSON : "Alex Sexton",
+            GENDER: "male"
+        }) ).to.eql('Alex Sexton added just himself to his group.');
+
+        expect( mf_func({
+            PLURAL_NUM_PEOPLE : 2,
+            PERSON : "Alex Sexton",
+            GENDER: "male"
+        }) ).to.eql('Alex Sexton added himself and one other person to his group.');
+
+        expect( mf_func({
+            PLURAL_NUM_PEOPLE : 3,
+            PERSON : "Alex Sexton",
+            GENDER: "male"
+        }) ).to.eql('Alex Sexton added himself and 2 other people to his group.');
+
+        expect( mf_func({
+            PLURAL_NUM_PEOPLE : 0,
+            PERSON : "Al Sexton"
+        }) ).to.eql('Al Sexton added no one to their group.');
+
+        expect( mf_func({
+            PLURAL_NUM_PEOPLE : 1,
+            PERSON : "Al Sexton"
+        }) ).to.eql('Al Sexton added just themself to their group.');
+
+        expect( mf_func({
+            PLURAL_NUM_PEOPLE : 2,
+            PERSON : "Al Sexton"
+        }) ).to.eql('Al Sexton added themself and one other person to their group.');
+
+        expect( mf_func({
+            PLURAL_NUM_PEOPLE : 3,
+            PERSON : "Al Sexton"
+        }) ).to.eql('Al Sexton added themself and 2 other people to their group.');
+
+      });
     });
-
-    it("throws an error when no `other` option is found - selects", function () {
-      var mf = new MessageFormat( 'en' );
-      expect(function(){ var x = mf.compile("{X, select, someoption{a}}"); }).to.throwError();
-    });
-
-    it("can parse complex, real-world strings", function () {
-      var input = "" +
-      "{PERSON} added {PLURAL_NUM_PEOPLE, plural, offset:1" +
-      "     =0 {no one}"+
-      "     =1 {just {GENDER, select, male {him} female {her} other{them}}self}"+
-      "    one {{GENDER, select, male {him} female {her} other{them}}self and one other person}"+
-      "  other {{GENDER, select, male {him} female {her} other{them}}self and # other people}"+
-      "} to {GENDER, select,"+
-      "   male {his}"+
-      " female {her}"+
-      "  other {their}"+
-      "} group.";
-
-      var mf = new MessageFormat( 'en' );
-      var mf_func = mf.compile( input );
-
-      expect( mf_func({
-          PLURAL_NUM_PEOPLE : 0,
-          PERSON : "Allie Sexton",
-          GENDER: "female"
-      }) ).to.eql('Allie Sexton added no one to her group.');
-
-      expect( mf_func({
-          PLURAL_NUM_PEOPLE : 1,
-          PERSON : "Allie Sexton",
-          GENDER: "female"
-      }) ).to.eql('Allie Sexton added just herself to her group.');
-
-      expect( mf_func({
-          PLURAL_NUM_PEOPLE : 2,
-          PERSON : "Allie Sexton",
-          GENDER: "female"
-      }) ).to.eql('Allie Sexton added herself and one other person to her group.');
-
-      expect( mf_func({
-          PLURAL_NUM_PEOPLE : 3,
-          PERSON : "Allie Sexton",
-          GENDER: "female"
-      }) ).to.eql('Allie Sexton added herself and 2 other people to her group.');
-
-      expect( mf_func({
-          PLURAL_NUM_PEOPLE : 0,
-          PERSON : "Alex Sexton",
-          GENDER: "male"
-      }) ).to.eql('Alex Sexton added no one to his group.');
-
-      expect( mf_func({
-          PLURAL_NUM_PEOPLE : 1,
-          PERSON : "Alex Sexton",
-          GENDER: "male"
-      }) ).to.eql('Alex Sexton added just himself to his group.');
-
-      expect( mf_func({
-          PLURAL_NUM_PEOPLE : 2,
-          PERSON : "Alex Sexton",
-          GENDER: "male"
-      }) ).to.eql('Alex Sexton added himself and one other person to his group.');
-
-      expect( mf_func({
-          PLURAL_NUM_PEOPLE : 3,
-          PERSON : "Alex Sexton",
-          GENDER: "male"
-      }) ).to.eql('Alex Sexton added himself and 2 other people to his group.');
-
-      expect( mf_func({
-          PLURAL_NUM_PEOPLE : 0,
-          PERSON : "Al Sexton"
-      }) ).to.eql('Al Sexton added no one to their group.');
-
-      expect( mf_func({
-          PLURAL_NUM_PEOPLE : 1,
-          PERSON : "Al Sexton"
-      }) ).to.eql('Al Sexton added just themself to their group.');
-
-      expect( mf_func({
-          PLURAL_NUM_PEOPLE : 2,
-          PERSON : "Al Sexton"
-      }) ).to.eql('Al Sexton added themself and one other person to their group.');
-
-      expect( mf_func({
-          PLURAL_NUM_PEOPLE : 3,
-          PERSON : "Al Sexton"
-      }) ).to.eql('Al Sexton added themself and 2 other people to their group.');
-
-    });
-
   });
 
 });
