7453859c8c7765d72185a9d36c2ee6963f35e94c
Eemeli Aro <eemeli@gmail.com>
2014-02-09 23:06:03 +0200

    updated examples

--- example/en/i18n.js
+++ example/en/i18n.js
@@ -5,12 +5,12 @@ var
 c=function(d){if(!d)throw new Error("MessageFormat: No data passed to function.")},
 n=function(d,k,o){if(isNaN(d[k]))throw new Error("MessageFormat: `"+k+"` isnt a number.");return d[k]-(o||0)},
 v=function(d,k){c(d);return d[k]},
-p=function(d,k,o,l,p){c(d);var v=p[d[k]]||p[MessageFormat.locale[l](d[k]-o)]||p.other;return"string"==typeof v?v:v(d)},
-s=function(d,k,p){c(d);var v=p[d[k]]||p.other;return"string"==typeof v?v:v(d)};
-window.i18n["colors"] = {}
-window.i18n["colors"]["red"] = function(d){return "red"}
-window.i18n["colors"]["blue"] = function(d){return "blue"}
-window.i18n["colors"]["green"] = function(d){return "green"}
-window.i18n["sub/folder/plural"] = {}
-window.i18n["sub/folder/plural"]["test"] = function(d){return "Your "+p(d,"NUM",0,"en",{"one":"message","other":"messages"})+" go here."}
+p=function(d,k,o,l,p){c(d);return p[d[k]]||p[MessageFormat.locale[l](d[k]-o)]||p.other},
+s=function(d,k,p){c(d);return p[d[k]]||p.other};
+window.i18n["colors"] = {
+"red":function(d){return "red"},
+"blue":function(d){return "blue"},
+"green":function(d){return "green"}}
+window.i18n["sub/folder/plural"] = {
+"test":function(d){return "Your "+p(d,"NUM",0,"en",{"one":"message","other":"messages"})+" go here."}}
 })();
--- example/fr/i18n.js
+++ example/fr/i18n.js
@@ -5,12 +5,12 @@ var
 c=function(d){if(!d)throw new Error("MessageFormat: No data passed to function.")},
 n=function(d,k,o){if(isNaN(d[k]))throw new Error("MessageFormat: `"+k+"` isnt a number.");return d[k]-(o||0)},
 v=function(d,k){c(d);return d[k]},
-p=function(d,k,o,l,p){c(d);var v=p[d[k]]||p[MessageFormat.locale[l](d[k]-o)]||p.other;return"string"==typeof v?v:v(d)},
-s=function(d,k,p){c(d);var v=p[d[k]]||p.other;return"string"==typeof v?v:v(d)};
-window.i18n["colors"] = {}
-window.i18n["colors"]["red"] = function(d){return "rouge"}
-window.i18n["colors"]["blue"] = function(d){return "bleu"}
-window.i18n["colors"]["green"] = function(d){return "vert"}
-window.i18n["sub/folder/plural"] = {}
-window.i18n["sub/folder/plural"]["test"] = function(d){return p(d,"NUM",0,"fr",{"one":"Votre message se trouve","other":"Vos messages se trouvent"})+" ici."}
+p=function(d,k,o,l,p){c(d);return p[d[k]]||p[MessageFormat.locale[l](d[k]-o)]||p.other},
+s=function(d,k,p){c(d);return p[d[k]]||p.other};
+window.i18n["colors"] = {
+"red":function(d){return "rouge"},
+"blue":function(d){return "bleu"},
+"green":function(d){return "vert"}}
+window.i18n["sub/folder/plural"] = {
+"test":function(d){return p(d,"NUM",0,"fr",{"one":"Votre message se trouve","other":"Vos messages se trouvent"})+" ici."}}
 })();
