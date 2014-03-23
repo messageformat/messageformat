67582f5688f4e3c9f00ed00653c9748703d6b1a2
Eemeli Aro <eemeli@gmail.com>
2014-03-23 17:23:31 +0200

    updated examples

--- example/en/i18n.js
+++ example/en/i18n.js
@@ -1,16 +1,22 @@
 (function(){ window.i18n || (window.i18n = {}) 
 var MessageFormat = { locale: {} };
 MessageFormat.locale.en=function(n){return n===1?"one":"other"}
+window.i18n.get=function(n,k,d){
+var m=this[n],f=function(k,d){return typeof m[k]=="function"?m[k](d):k in m?m[k]:k};
+return !m?null
+:typeof k=="undefined"?f
+:typeof d=="undefined"?typeof m[k]=="function"?m[k]:function(){return m[k]}
+:f(k,d)}
 var
 c=function(d){if(!d)throw new Error("MessageFormat: No data passed to function.")},
 n=function(d,k,o){if(isNaN(d[k]))throw new Error("MessageFormat: `"+k+"` isnt a number.");return d[k]-(o||0)},
 v=function(d,k){c(d);return d[k]},
-p=function(d,k,o,l,p){c(d);return p[d[k]]||p[MessageFormat.locale[l](d[k]-o)]||p.other},
-s=function(d,k,p){c(d);return p[d[k]]||p.other};
+p=function(d,k,o,l,p){c(d);return d[k] in p?p[d[k]]:(k=MessageFormat.locale[l](d[k]-o),k in p?p[k]:p.other)},
+s=function(d,k,p){c(d);return d[k] in p?p[d[k]]:p.other};
 window.i18n["colors"] = {
-"red":function(d){return "red"},
-"blue":function(d){return "blue"},
-"green":function(d){return "green"}}
+"red":"red",
+"blue":"blue",
+"green":"green"}
 window.i18n["sub/folder/plural"] = {
 "test":function(d){return "Your "+p(d,"NUM",0,"en",{"one":"message","other":"messages"})+" go here."}}
 })();
