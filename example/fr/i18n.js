1eccb3da45a4510d1ce2a5054a30dcba37094db3
Eemeli Aro <eemeli@gmail.com>
2015-02-11 21:43:39 +0200

    Internal variable & runtime cleanup

--- example/fr/i18n.js
+++ example/fr/i18n.js
@@ -1,11 +1,8 @@
 (function(G){var
-_n=function(v,o){if(isNaN(v))throw new Error("'"+v+"' isn't a number.");return v-(o||0)},
-_p=function(v,o,l,p,s){return v in p?p[v]:(v=l(v-o,s),v in p?p[v]:p.other)},
-_s=function(v,p){return v in p?p[v]:p.other},
-pf={"fr":function(n,ord) {
-  if (ord) return (n == 1) ? 'one' : 'other';
-  return (n >= 0 && n < 2) ? 'one' : 'other';
-}},
+n=function(v,o){ if (isNaN(v)) throw new Error("'"+v+"' isn't a number."); return v - (o||0) },
+p=function(v,o,l,p,s){ return v in p ? p[v] : ( v = l(v-o,s), v in p ? p[v] : p.other ) },
+s=function(v,p){ return v in p ? p[v] : p.other },
+pf={"fr":function(n,ord) { if (ord) return (n == 1) ? 'one' : 'other'; return (n >= 0 && n < 2) ? 'one' : 'other'; }},
 fmt={};
 
 G["i18n"] = {
@@ -14,5 +11,5 @@ red:function(d){return "rouge"},
 blue:function(d){return "bleu"},
 green:function(d){return "vert"}},
 "sub/folder/plural":{
-test:function(d){return _p(d["NUM"],0,pf["fr"],{one:"Votre message se trouve",other:"Vos messages se trouvent"})+" ici."}}}
+test:function(d){return p(d["NUM"],0,pf["fr"],{one:"Votre message se trouve",other:"Vos messages se trouvent"})+" ici."}}}
 })(this);
