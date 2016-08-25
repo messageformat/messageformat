b258bc865de4107053b31825d490cb0923cc8fb4
Clayton Watts <cletusw@gmail.com>
2016-08-25 16:51:48 -0600

    Mark as cacheable and use loader chaining shortcut values

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ var MessageFormat = require('messageformat');
 module.exports = function(content) {
   var query = loaderUtils.parseQuery(this.query);
   var locale = query.locale || 'en';
-  var messages = this.exec(content);
-  var messageFunctions = new MessageFormat(locale).compile(messages).toString('module.exports');
-  return messageFunctions;
+  var messages = typeof this.inputValue === 'object' ? this.inputValue : this.exec(content);
+  var messageFunctions = new MessageFormat(locale).compile(messages);
+
+  this.cacheable && this.cacheable();
+  this.value = messageFunctions;
+
+  return messageFunctions.toString('module.exports');
 };
