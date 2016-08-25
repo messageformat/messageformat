568777cd7f4b07ffbb02954b513762f1df9a853f
Clayton Watts <cletusw@gmail.com>
2016-08-25 17:01:48 -0600

    Forgot that typeof an Array is 'object'

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var MessageFormat = require('messageformat');
 module.exports = function(content) {
   var query = loaderUtils.parseQuery(this.query);
   var locale = query.locale || 'en';
-  var messages = typeof this.inputValue === 'object' ? this.inputValue : this.exec(content);
+  var messages = (Array.isArray(this.inputValue) && typeof this.inputValue[0] === 'object') ? this.inputValue[0] : this.exec(content);
   var messageFunctions = new MessageFormat(locale).compile(messages);
 
   this.cacheable && this.cacheable();
