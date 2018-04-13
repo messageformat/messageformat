332a18af7c0e683a7671b9c46735d768f958e67d
Patrick Desjardins <pdesjardins@netflix.com>
2018-04-13 09:31:15 -0700

    Add SrcMessage, Formatter, an empty constructor for MessageFormat

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,21 @@
 declare module "messageformat" {
+    type Msg = (params: {}) => string;
+    type Formatter = (val: any, lc: string, arg?: string) => string;
+    type SrcMessage = string | SrcObject;
+
+    interface SrcObject {
+        [key: string]: SrcMessage;
+    }
     class MessageFormat {
         constructor(message: { [pluralFuncs: string]: Function });
         constructor(message: string[]);
         constructor(message: string);
-        addFormatters: (format: {}) => MessageFormat;
+        constructor();
+        addFormatters: (format: { [name: string]: Formatter }) => MessageFormat;
         disablePluralKeyChecks: () => MessageFormat;
         setBiDiSupport: (enable: boolean) => MessageFormat;
         setStrictNumberSign: (enable: boolean) => MessageFormat;
-        compile: (messages: string, locale?: string) => Msg;
+        compile: (messages: SrcMessage, locale?: string) => Msg;
     }
-
-    type Msg = (params: {}) => string;
     export = MessageFormat;
 }
