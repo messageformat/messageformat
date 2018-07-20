08b4b459288c945e392636ae134a5d6968bb373b
Barz <max-richard.barz@siemens.com>
2018-07-20 16:13:41 +0200

    type definition that should work with es6 style imports

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-declare module "messageformat" {
+declare namespace messageformat {
     type Msg = (params: {}) => string;
     type Formatter = (val: any, lc: string, arg?: string) => string;
     type SrcMessage = string | SrcObject;
@@ -6,16 +6,18 @@ declare module "messageformat" {
     interface SrcObject {
         [key: string]: SrcMessage;
     }
-    class MessageFormat {
-        constructor(message: { [pluralFuncs: string]: Function });
-        constructor(message: string[]);
-        constructor(message: string);
-        constructor();
-        addFormatters: (format: { [name: string]: Formatter }) => MessageFormat;
-        disablePluralKeyChecks: () => MessageFormat;
-        setBiDiSupport: (enable: boolean) => MessageFormat;
-        setStrictNumberSign: (enable: boolean) => MessageFormat;
-        compile: (messages: SrcMessage, locale?: string) => Msg;
-    }
-    export = MessageFormat;
 }
+
+declare class messageformat {
+    constructor(message: { [pluralFuncs: string]: Function });
+    constructor(message: string[]);
+    constructor(message: string);
+    constructor();
+    addFormatters: (format: { [name: string]: messageformat.Formatter }) => messageformat;
+    disablePluralKeyChecks: () => messageformat;
+    setBiDiSupport: (enable: boolean) => messageformat;
+    setStrictNumberSign: (enable: boolean) => messageformat;
+    compile: (messages: messageformat.SrcMessage, locale?: string) => messageformat.Msg;
+}
+
+export = messageformat;
