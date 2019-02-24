1d2958f4014a5c57f09a7a6ff7783e65309bff2c
Eemeli Aro <eemeli@gmail.com>
2019-02-24 17:11:14 +0200

    feat(scripts): Add a start script, for ease of development

---
+++ scripts/start.js
@@ -0,0 +1,5 @@
+global.convert = require('../packages/convert');
+global.MessageFormat = require('../packages/messageformat');
+global.parse = require('../packages/parser').parse;
+
+global.mf = new global.MessageFormat('en');
