5530b5d0246f3a7e6e2729f36a60d1100b8c6a8e
Eemeli Aro <eemeli@gmail.com>
2019-04-18 22:58:45 +0300

    test: Drop fat-arrow use to support testing on IE 11

--- test/formatters.js
+++ test/formatters.js
@@ -8,26 +8,26 @@ function dropBiDi(str) {
   return str.replace(/[\u200e\u200f]/g, '');
 }
 
-describe('Formatters', () => {
-  describe('date', () => {
+describe('Formatters', function() {
+  describe('date', function() {
     let mf;
-    beforeEach(() => {
+    beforeEach(function() {
       mf = new MessageFormat(['en', 'fi']);
     });
 
-    it('default', () => {
+    it('default', function() {
       const msg = mf.compile('Today is {T, date}');
       const data = { T: new Date(2016, 1, 21) };
       expect(dropBiDi(msg(data))).to.eql('Today is Feb 21, 2016');
     });
 
-    it('set locale', () => {
+    it('set locale', function() {
       const msg = mf.compile('Tänään on {T, date}', 'fi');
       const data = { T: new Date(2016, 1, 21) };
       expect(dropBiDi(msg(data))).to.match(/^Tänään on .*2016/);
     });
 
-    it('argument', () => {
+    it('argument', function() {
       const msg = mf.compile('Unix time started on {T, date, full}');
       const data = { T: 0 };
       expect(dropBiDi(msg(data))).to.be.oneOf([
@@ -36,7 +36,7 @@ describe('Formatters', () => {
       ]);
     });
 
-    it('complex', () => {
+    it('complex', function() {
       const msg = mf.compile('{sys} became operational on {d0, date, short}');
       const data = { sys: 'HAL 9000', d0: new Date(1999, 0, 12) };
       expect(dropBiDi(msg(data))).to.eql(
@@ -45,57 +45,57 @@ describe('Formatters', () => {
     });
   });
 
-  describe('duration', () => {
+  describe('duration', function() {
     let mf;
-    beforeEach(() => {
+    beforeEach(function() {
       mf = new MessageFormat();
     });
 
-    it('default', () => {
+    it('default', function() {
       const msg = mf.compile('It has been {D, duration}');
       const data = { D: 123 };
       expect(msg(data)).to.eql('It has been 2:03');
     });
 
-    it('complex', () => {
+    it('complex', function() {
       const msg = mf.compile('Countdown: {D, duration}');
       const data = { D: -151200.42 };
       expect(msg(data)).to.eql('Countdown: -42:00:00.420');
     });
   });
 
-  describe('number', () => {
+  describe('number', function() {
     let mf;
-    beforeEach(() => {
+    beforeEach(function() {
       mf = new MessageFormat('en');
     });
 
-    it('integer', () => {
+    it('integer', function() {
       const msg = mf.compile('{N} is almost {N, number, integer}');
       const data = { N: 3.14 };
       expect(msg(data)).to.eql('3.14 is almost 3');
     });
 
-    it('percent', () => {
+    it('percent', function() {
       const msg = mf.compile('{P, number, percent} complete');
       const data = { P: 0.99 };
       expect(msg(data)).to.eql('99% complete');
     });
 
-    it('default currency', () => {
+    it('default currency', function() {
       const msg = mf.compile('The total is {V, number, currency}.');
       const data = { V: 5.5 };
       expect(msg(data)).to.eql('The total is $5.50.');
     });
 
-    it('currency', () => {
+    it('currency', function() {
       mf.currency = 'EUR';
       const msg = mf.compile('The total is {V, number, currency}.');
       const data = { V: 5.5 };
       expect(msg(data)).to.eql('The total is €5.50.');
     });
 
-    it('currency:GBP', () => {
+    it('currency:GBP', function() {
       mf.currency = 'EUR';
       const msg = mf.compile('The total is {V, number, currency:GBP}.');
       const data = { V: 5.5 };
@@ -103,13 +103,13 @@ describe('Formatters', () => {
     });
   });
 
-  describe('time', () => {
+  describe('time', function() {
     let mf;
-    beforeEach(() => {
+    beforeEach(function() {
       mf = new MessageFormat(['en', 'fi']);
     });
 
-    it('default', () => {
+    it('default', function() {
       const msg = mf.compile('The time is now {T, time}');
       const data = { T: 978384385000 };
       expect(dropBiDi(msg(data))).to.match(
@@ -117,13 +117,13 @@ describe('Formatters', () => {
       );
     });
 
-    it('set locale', () => {
+    it('set locale', function() {
       const msg = mf.compile('Kello on nyt {T, time}', 'fi');
       const data = { T: 978384385000 };
       expect(dropBiDi(msg(data))).to.match(/^Kello on nyt \d\d?.\d\d.25/);
     });
 
-    it('full time & date', () => {
+    it('full time & date', function() {
       const msg = mf.compile(
         'The Eagle landed at {T, time, full} on {T, date, full}'
       );
@@ -136,26 +136,26 @@ describe('Formatters', () => {
     });
   });
 
-  describe('Custom formatters', () => {
+  describe('Custom formatters', function() {
     let mf;
-    beforeEach(() => {
+    beforeEach(function() {
       mf = new MessageFormat('en');
     });
 
-    it('should throw an error when using an undefined formatting function', () => {
-      expect(() => {
+    it('should throw an error when using an undefined formatting function', function() {
+      expect(function() {
         mf.compile('This is {VAR,uppercase}.');
       }).to.throw();
     });
 
-    it('should use formatting functions - set in MessageFormat.formatters', () => {
+    it('should use formatting functions - set in MessageFormat.formatters', function() {
       let msg = mf.compile('The date is {VAR,date}.');
       expect(msg({ VAR: '2010-12-31' })).to.contain('2010');
       msg = mf.compile('Countdown: {VAR, duration}.');
       expect(msg({ VAR: -151200.42 })).to.eql('Countdown: -42:00:00.420.');
     });
 
-    it('should use formatting functions - set by #addFormatters()', () => {
+    it('should use formatting functions - set by #addFormatters()', function() {
       mf.addFormatters({
         uppercase: function(v) {
           return v.toUpperCase();
@@ -165,7 +165,7 @@ describe('Formatters', () => {
       expect(msg({ VAR: 'big' })).to.eql('This is BIG.');
     });
 
-    it('should use formatting functions for object input - set by #addFormatters()', () => {
+    it('should use formatting functions for object input - set by #addFormatters()', function() {
       mf.addFormatters({
         uppercase: function(v) {
           return v.toUpperCase();
@@ -175,8 +175,8 @@ describe('Formatters', () => {
       expect(msg[0]({ VAR: 'big' })).to.eql('This is BIG.');
     });
 
-    describe('arguments', () => {
-      beforeEach(() => {
+    describe('arguments', function() {
+      beforeEach(function() {
         mf = new MessageFormat('en');
         mf.addFormatters({
           arg: function(v, lc, arg) {
@@ -185,19 +185,19 @@ describe('Formatters', () => {
         });
       });
 
-      it('basic string', () => {
+      it('basic string', function() {
         const msg = mf.compile('This is {_, arg, X, Y }.');
         expect(msg({})).to.eql('This is X, Y.');
       });
 
-      it('select', () => {
+      it('select', function() {
         const msg = mf.compile(
           'This is {_, arg, {VAR, select, x{X} other{Y}}}.'
         );
         expect(msg({ VAR: 'x' })).to.eql('This is X.');
       });
 
-      it('# in plural', () => {
+      it('# in plural', function() {
         const msg = mf.compile(
           'This is {VAR, plural, one{} other{{_, arg, #}}}.'
         );
