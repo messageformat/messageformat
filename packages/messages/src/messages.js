64de7badec49980870f8a75c62d6db4f810e6bb8
Eemeli Aro <eemeli@gmail.com>
2019-08-04 15:09:31 +0300

    feat(messages): Refactor as an ES6 class

--- packages/messages/src/messages.js
+++ packages/messages/src/messages.js
@@ -10,10 +10,10 @@
  * @param {string|null} [defaultLocale] If not defined, default and initial locale is the first entry of `locales`
  *
  * @example
- * var fs = require('fs');
- * var MessageFormat = require('messageformat');
- * var mf = new MessageFormat(['en', 'fi']);
- * var msgSet = {
+ * const fs = require('fs');
+ * const MessageFormat = require('messageformat');
+ * const mf = new MessageFormat(['en', 'fi']);
+ * const msgSet = {
  *   en: {
  *     a: 'A {TYPE} example.',
  *     b: 'This has {COUNT, plural, one{one user} other{# users}}.',
@@ -26,14 +26,14 @@
  *     e: 'Minä puhun vain suomea.'
  *   }
  * };
- * var cfStr = mf.compile(msgSet).toString('module.exports');
+ * const cfStr = mf.compile(msgSet).toString('module.exports');
  * fs.writeFileSync('messages.js', cfStr);
  *
  * ...
  *
- * var Messages = require('messageformat-messages');
- * var msgData = require('./messages');
- * var messages = new Messages(msgData, 'en');
+ * const Messages = require('messageformat-messages');
+ * const msgData = require('./messages');
+ * const messages = new Messages(msgData, 'en');
  *
  * messages.hasMessage('a')                // true
  * messages.hasObject('c')                 // true
@@ -52,15 +52,19 @@
  * messages.get('b', { COUNT: 3 })         // 'Tällä on 3 käyttäjää.'
  * messages.get('c').d({ P: 0.628 })       // 'We have 63% code coverage.'
  */
-function Messages(locales, defaultLocale) {
-  this._data = {};
-  this._fallback = {};
-  Object.keys(locales).forEach(function(lc) {
-    if (lc !== 'toString') {
-      this._data[lc] = locales[lc];
-      if (typeof defaultLocale === 'undefined') defaultLocale = lc;
-    }
-  }, this);
+module.exports = class Messages {
+  constructor(locales, defaultLocale) {
+    this._data = {};
+    this._fallback = {};
+    Object.keys(locales).forEach(lc => {
+      if (lc !== 'toString') {
+        this._data[lc] = locales[lc];
+        if (typeof defaultLocale === 'undefined') defaultLocale = lc;
+      }
+    });
+    this.locale = defaultLocale;
+    this._defaultLocale = this._locale;
+  }
 
   /**
    * List of available locales
@@ -68,11 +72,9 @@ function Messages(locales, defaultLocale) {
    * @memberof Messages
    * @member {string[]} availableLocales
    */
-  Object.defineProperty(this, 'availableLocales', {
-    get: function() {
-      return Object.keys(this._data);
-    }
-  });
+  get availableLocales() {
+    return Object.keys(this._data);
+  }
 
   /**
    * Current locale
@@ -84,15 +86,12 @@ function Messages(locales, defaultLocale) {
    * @memberof Messages
    * @member {string|null} locale
    */
-  Object.defineProperty(this, 'locale', {
-    get: function() {
-      return this._locale;
-    },
-    set: function(lc) {
-      this._locale = this.resolveLocale(lc);
-    }
-  });
-  this.locale = defaultLocale;
+  get locale() {
+    return this._locale;
+  }
+  set locale(lc) {
+    this._locale = this.resolveLocale(lc);
+  }
 
   /**
    * Default fallback locale
@@ -104,180 +103,175 @@ function Messages(locales, defaultLocale) {
    * @memberof Messages
    * @member {string|null} defaultLocale
    */
-  Object.defineProperty(this, 'defaultLocale', {
-    get: function() {
-      return this._defaultLocale;
-    },
-    set: function(lc) {
-      this._defaultLocale = this.resolveLocale(lc);
-    }
-  });
-  this._defaultLocale = this._locale;
-}
-
-module.exports = Messages;
-
-/**
- * Add new messages to the accessor; useful if loading data dynamically
- *
- * The locale code `lc` should be an exact match for the locale being updated,
- * or empty to default to the current locale. Use {@link #resolveLocale} for
- * resolving partial locale strings.
- *
- * If `keypath` is empty, adds or sets the complete message object for the
- * corresponding locale. If any keys in `keypath` do not exist, a new object
- * will be created at that key.
- *
- * @param {function|object} data Hierarchical map of keys to functions, or a
- *   single message function
- * @param {string} [lc] If empty or undefined, defaults to `this.locale`
- * @param {string[]} [keypath] The keypath being added
- * @returns {Messages} The Messages instance, to allow for chaining
- */
-Messages.prototype.addMessages = function(data, lc, keypath) {
-  if (!lc) lc = this.locale;
-  if (typeof data !== 'function') {
-    data = Object.keys(data).reduce(function(map, key) {
-      if (key !== 'toString') map[key] = data[key];
-      return map;
-    }, {});
+  get defaultLocale() {
+    return this._defaultLocale;
   }
-  if (Array.isArray(keypath) && keypath.length > 0) {
-    var parent = this._data[lc];
-    for (var i = 0; i < keypath.length - 1; ++i) {
-      var key = keypath[i];
-      if (!parent[key]) parent[key] = {};
-      parent = parent[key];
-    }
-    parent[keypath[keypath.length - 1]] = data;
-  } else {
-    this._data[lc] = data;
+  set defaultLocale(lc) {
+    this._defaultLocale = this.resolveLocale(lc);
   }
-  return this;
-};
 
-/**
- * Resolve `lc` to the key of an available locale or `null`, allowing for
- * partial matches. For example, with an `en` locale defined, it will be
- * selected by `messages.defaultLocale = 'en-US'` and vice versa.
- *
- * @param {string} lc Locale code
- * @returns {string|null}
- */
-Messages.prototype.resolveLocale = function(lc) {
-  if (this._data[lc]) return lc;
-  if (lc) {
-    var l = String(lc);
-    while ((l = l.replace(/[-_]?[^-_]*$/, ''))) {
-      if (this._data[l]) return l;
+  /**
+   * Add new messages to the accessor; useful if loading data dynamically
+   *
+   * The locale code `lc` should be an exact match for the locale being updated,
+   * or empty to default to the current locale. Use {@link #resolveLocale} for
+   * resolving partial locale strings.
+   *
+   * If `keypath` is empty, adds or sets the complete message object for the
+   * corresponding locale. If any keys in `keypath` do not exist, a new object
+   * will be created at that key.
+   *
+   * @param {function|object} data Hierarchical map of keys to functions, or a
+   *   single message function
+   * @param {string} [lc] If empty or undefined, defaults to `this.locale`
+   * @param {string[]} [keypath] The keypath being added
+   * @returns {Messages} The Messages instance, to allow for chaining
+   */
+  addMessages(data, lc, keypath) {
+    if (!lc) lc = this.locale;
+    if (typeof data !== 'function') {
+      data = Object.keys(data).reduce((map, key) => {
+        if (key !== 'toString') map[key] = data[key];
+        return map;
+      }, {});
     }
-    var ll = this.availableLocales;
-    var re = new RegExp('^' + lc + '[-_]');
-    for (var i = 0; i < ll.length; ++i) {
-      if (re.test(ll[i])) return ll[i];
+    if (Array.isArray(keypath) && keypath.length > 0) {
+      let parent = this._data[lc];
+      for (let i = 0; i < keypath.length - 1; ++i) {
+        const key = keypath[i];
+        if (!parent[key]) parent[key] = {};
+        parent = parent[key];
+      }
+      parent[keypath[keypath.length - 1]] = data;
+    } else {
+      this._data[lc] = data;
     }
+    return this;
   }
-  return null;
-};
 
-/**
- * Get the list of fallback locales
- * @param {string} [lc] If empty or undefined, defaults to `this.locale`
- * @returns {string[]}
- */
-Messages.prototype.getFallback = function(lc) {
-  if (!lc) lc = this.locale;
-  return (
-    this._fallback[lc] ||
-    (lc === this.defaultLocale || !this.defaultLocale
-      ? []
-      : [this.defaultLocale])
-  );
-};
+  /**
+   * Resolve `lc` to the key of an available locale or `null`, allowing for
+   * partial matches. For example, with an `en` locale defined, it will be
+   * selected by `messages.defaultLocale = 'en-US'` and vice versa.
+   *
+   * @param {string} lc Locale code
+   * @returns {string|null}
+   */
+  resolveLocale(lc) {
+    if (this._data[lc]) return lc;
+    if (lc) {
+      let l = String(lc);
+      while ((l = l.replace(/[-_]?[^-_]*$/, ''))) {
+        if (this._data[l]) return l;
+      }
+      const ll = this.availableLocales;
+      const re = new RegExp('^' + lc + '[-_]');
+      for (let i = 0; i < ll.length; ++i) {
+        if (re.test(ll[i])) return ll[i];
+      }
+    }
+    return null;
+  }
 
-/**
- * Set the fallback locale or locales for `lc`
- *
- * To disable fallback for the locale, use `setFallback(lc, [])`.
- * To use the default fallback, use `setFallback(lc, null)`.
- *
- * @param {string} lc
- * @param {string[]|null} fallback
- * @returns {Messages} The Messages instance, to allow for chaining
- */
-Messages.prototype.setFallback = function(lc, fallback) {
-  this._fallback[lc] = Array.isArray(fallback) ? fallback : null;
-  return this;
-};
+  /**
+   * Get the list of fallback locales
+   * @param {string} [lc] If empty or undefined, defaults to `this.locale`
+   * @returns {string[]}
+   */
+  getFallback(lc) {
+    if (!lc) lc = this.locale;
+    return (
+      this._fallback[lc] ||
+      (lc === this.defaultLocale || !this.defaultLocale
+        ? []
+        : [this.defaultLocale])
+    );
+  }
 
-/**
- * Check if `key` is a message function for the locale
- *
- * `key` may be a `string` for functions at the root level, or `string[]` for
- * accessing hierarchical objects. If an exact match is not found and `fallback`
- * is true, the fallback locales are checked for the first match.
- *
- * @param {string|string[]} key The key or keypath being sought
- * @param {string} [lc] If empty or undefined, defaults to `this.locale`
- * @param {boolean} [fallback=false] If true, also checks fallback locales
- * @returns {boolean}
- */
-Messages.prototype.hasMessage = function(key, lc, fallback) {
-  if (!lc) lc = this.locale;
-  var fb = fallback ? this.getFallback(lc) : null;
-  return _has(this._data, lc, key, fb, 'function');
-};
+  /**
+   * Set the fallback locale or locales for `lc`
+   *
+   * To disable fallback for the locale, use `setFallback(lc, [])`.
+   * To use the default fallback, use `setFallback(lc, null)`.
+   *
+   * @param {string} lc
+   * @param {string[]|null} fallback
+   * @returns {Messages} The Messages instance, to allow for chaining
+   */
+  setFallback(lc, fallback) {
+    this._fallback[lc] = Array.isArray(fallback) ? fallback : null;
+    return this;
+  }
 
-/**
- * Check if `key` is a message object for the locale
- *
- * `key` may be a `string` for functions at the root level, or `string[]` for
- * accessing hierarchical objects. If an exact match is not found and `fallback`
- * is true, the fallback locales are checked for the first match.
- *
- * @param {string|string[]} key The key or keypath being sought
- * @param {string} [lc] If empty or undefined, defaults to `this.locale`
- * @param {boolean} [fallback=false] If true, also checks fallback locales
- * @returns {boolean}
- */
-Messages.prototype.hasObject = function(key, lc, fallback) {
-  if (!lc) lc = this.locale;
-  var fb = fallback ? this.getFallback(lc) : null;
-  return _has(this._data, lc, key, fb, 'object');
-};
+  /**
+   * Check if `key` is a message function for the locale
+   *
+   * `key` may be a `string` for functions at the root level, or `string[]` for
+   * accessing hierarchical objects. If an exact match is not found and `fallback`
+   * is true, the fallback locales are checked for the first match.
+   *
+   * @param {string|string[]} key The key or keypath being sought
+   * @param {string} [lc] If empty or undefined, defaults to `this.locale`
+   * @param {boolean} [fallback=false] If true, also checks fallback locales
+   * @returns {boolean}
+   */
+  hasMessage(key, lc, fallback) {
+    if (!lc) lc = this.locale;
+    const fb = fallback ? this.getFallback(lc) : null;
+    return _has(this._data, lc, key, fb, 'function');
+  }
 
-/**
- * Get the message or object corresponding to `key`
- *
- * `key` may be a `string` for functions at the root level, or `string[]` for
- * accessing hierarchical objects. If an exact match is not found, the fallback
- * locales are checked for the first match.
- *
- * If `key` maps to a message function, it will be called with `props`. If it
- * maps to an object, the object is returned directly.
- *
- * @param {string|string[]} key The key or keypath being sought
- * @param {object} [props] Optional properties passed to the function
- * @param {string} [lc] If empty or undefined, defaults to `this.locale`
- * @returns {string|Object<string,function|object>}
- */
-Messages.prototype.get = function(key, props, lc) {
-  if (!lc) lc = this.locale;
-  var msg = _get(this._data[lc], key);
-  if (msg) return typeof msg == 'function' ? msg(props) : msg;
-  var fb = this.getFallback(lc);
-  for (var i = 0; i < fb.length; ++i) {
-    msg = _get(this._data[fb[i]], key);
+  /**
+   * Check if `key` is a message object for the locale
+   *
+   * `key` may be a `string` for functions at the root level, or `string[]` for
+   * accessing hierarchical objects. If an exact match is not found and `fallback`
+   * is true, the fallback locales are checked for the first match.
+   *
+   * @param {string|string[]} key The key or keypath being sought
+   * @param {string} [lc] If empty or undefined, defaults to `this.locale`
+   * @param {boolean} [fallback=false] If true, also checks fallback locales
+   * @returns {boolean}
+   */
+  hasObject(key, lc, fallback) {
+    if (!lc) lc = this.locale;
+    const fb = fallback ? this.getFallback(lc) : null;
+    return _has(this._data, lc, key, fb, 'object');
+  }
+
+  /**
+   * Get the message or object corresponding to `key`
+   *
+   * `key` may be a `string` for functions at the root level, or `string[]` for
+   * accessing hierarchical objects. If an exact match is not found, the fallback
+   * locales are checked for the first match.
+   *
+   * If `key` maps to a message function, it will be called with `props`. If it
+   * maps to an object, the object is returned directly.
+   *
+   * @param {string|string[]} key The key or keypath being sought
+   * @param {object} [props] Optional properties passed to the function
+   * @param {string} [lc] If empty or undefined, defaults to `this.locale`
+   * @returns {string|Object<string,function|object>}
+   */
+  get(key, props, lc) {
+    if (!lc) lc = this.locale;
+    let msg = _get(this._data[lc], key);
     if (msg) return typeof msg == 'function' ? msg(props) : msg;
+    const fb = this.getFallback(lc);
+    for (let i = 0; i < fb.length; ++i) {
+      msg = _get(this._data[fb[i]], key);
+      if (msg) return typeof msg == 'function' ? msg(props) : msg;
+    }
+    return key;
   }
-  return key;
 };
 
 /** @private */
 function _get(obj, key) {
   if (!obj) return null;
   if (Array.isArray(key)) {
-    for (var i = 0; i < key.length; ++i) {
+    for (let i = 0; i < key.length; ++i) {
       obj = obj[key[i]];
       if (!obj) return null;
     }
@@ -288,13 +282,15 @@ function _get(obj, key) {
 
 /** @private */
 function _has(data, lc, key, fallback, type) {
-  var msg = _get(data[lc], key);
+  let msg = _get(data[lc], key);
   if (msg) return typeof msg === type;
   if (fallback) {
-    for (var i = 0; i < fallback.length; ++i) {
+    for (let i = 0; i < fallback.length; ++i) {
       msg = _get(data[fallback[i]], key);
       if (msg) return typeof msg === type;
     }
   }
   return false;
 }
+
+module.exports = Messages;
