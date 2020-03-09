32e52af52d7bdbc475cd127d049119fd8a8008ae
Eemeli Aro <eemeli@gmail.com>
2020-03-09 02:03:42 +0200

    feat(build): Replace Webpack with Rollup as messageformat bundler

--- packages/messageformat/compile-module.js
+++ packages/messageformat/compile-module.js
@@ -1,1 +1,1 @@
-module.exports = require('./lib/compile-module').default;
+module.exports = require('./lib/compile-module');
--- packages/messageformat/rollup.config.js
+++ packages/messageformat/rollup.config.js
@@ -1,9 +1,20 @@
+import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
-export default {
-  output: { format: 'cjs' },
+const nodeLib = {
+  input: {
+    compiler: 'src/compiler.js',
+    messageformat: 'src/messageformat.js',
+    'compile-module': 'src/compile-module.js'
+  },
+  output: { dir: 'lib/', format: 'cjs' },
   external: [
+    './compiler',
+    'make-plural/cardinals',
+    'make-plural/pluralCategories',
+    'make-plural/plurals',
+    // bundling messageformat-number-skeleton
     'messageformat-parser',
     'messageformat-runtime',
     'messageformat-runtime/lib/formatters',
@@ -11,3 +22,20 @@ export default {
   ],
   plugins: [resolve(), babel()]
 };
+
+const browserTargets = '> 0.5%, last 2 versions, Firefox ESR, not dead';
+const browserBundle = {
+  input: 'src/messageformat.js',
+  output: {
+    file: 'messageformat.js',
+    format: 'umd',
+    name: 'MessageFormat'
+  },
+  plugins: [
+    resolve(),
+    babel({ presets: [['@babel/preset-env', { targets: browserTargets }]] }),
+    commonjs()
+  ]
+};
+
+export default [nodeLib, browserBundle];
