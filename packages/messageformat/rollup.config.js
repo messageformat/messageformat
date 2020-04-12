b5cea016860e5721c44e3371de285e5d403c9e8c
Eemeli Aro <eemeli@gmail.com>
2020-04-12 21:24:08 +0300

    feat(messageformat): Switch build from Babel to TypeScript
    BREAKING CHANGE: While the transpiled JavaScript appears to be
    practically identical after this change, the TypeScript typings are now
    generated from actual source files, and are different from the previous
    ones.

--- packages/messageformat/rollup.config.js
+++ packages/messageformat/rollup.config.js
@@ -1,12 +1,12 @@
 import commonjs from '@rollup/plugin-commonjs';
 import resolve from '@rollup/plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import typescript from '@rollup/plugin-typescript';
 
 const nodeLib = {
   input: {
-    compiler: 'src/compiler.js',
-    messageformat: 'src/messageformat.js',
-    'compile-module': 'src/compile-module.js'
+    compiler: 'src/compiler.ts',
+    messageformat: 'src/messageformat.ts',
+    'compile-module': 'src/compile-module.ts'
   },
   output: { dir: 'lib/', format: 'cjs' },
   external: [
@@ -14,28 +14,24 @@ const nodeLib = {
     'make-plural/cardinals',
     'make-plural/pluralCategories',
     'make-plural/plurals',
+    // bundling messageformat-date-skeleton
     // bundling messageformat-number-skeleton
     'messageformat-parser',
     'messageformat-runtime',
     'messageformat-runtime/lib/formatters',
     'safe-identifier'
   ],
-  plugins: [resolve(), babel()]
+  plugins: [resolve(), typescript()]
 };
 
-const browserTargets = '> 0.5%, last 2 versions, Firefox ESR, not dead';
 const browserBundle = {
-  input: 'src/messageformat.js',
+  input: 'src/messageformat.ts',
   output: {
     file: 'messageformat.js',
     format: 'umd',
     name: 'MessageFormat'
   },
-  plugins: [
-    resolve(),
-    babel({ presets: [['@babel/preset-env', { targets: browserTargets }]] }),
-    commonjs()
-  ]
+  plugins: [resolve(), typescript({ target: 'ES5' }), commonjs()]
 };
 
 export default [nodeLib, browserBundle];
