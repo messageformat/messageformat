f70a5f5bd2590e995a53f9e44e1a05268af12d3a
Eemeli Aro <eemeli@gmail.com>
2020-04-12 15:10:50 +0300

    build(website): Add subdirectory for v3 docs

--- packages/website/publish.sh
+++ packages/website/publish.sh
@@ -7,12 +7,12 @@ ${GSED:-"sed"} -i '
     s/tutorial/page/g
     s/ Tutorial:/:/g
     s/"index\.html"/".\/"/g
-    s/"\([^"\/]\+\)\.html"/"\1"/g' docs/*.html
+    s/"\([^"\/]\+\)\.html\(["#]\)/"\1\2/g' docs/*.html
 
 for f in docs/tutorial*; do
     mv $f ${f/tutorial/page}
 done
 
 cp -r logo docs/
-rm -rf ../../docs/
-mv docs ../../
+rm -rf ../../docs/v3
+mv docs ../../docs/v3
