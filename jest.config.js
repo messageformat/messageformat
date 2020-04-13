c9a2597ef79b1180ec917499054bb49788ab0332
Eemeli Aro <eemeli@gmail.com>
2020-04-13 17:39:35 +0300

    test: Port test/messages.js to TS & Jest

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,9 @@ module.exports = {
   coveragePathIgnorePatterns: ['/node_modules/', '/fixtures/'],
   moduleFileExtensions: ['mjs', ...defaults.moduleFileExtensions],
   moduleNameMapper: {
+    '^messageformat$': '<rootDir>/packages/messageformat/src/messageformat.ts',
+    '^messageformat/compile-module$':
+      '<rootDir>/packages/messageformat/src/compile-module.ts',
     '^messageformat-runtime$': '<rootDir>/packages/runtime/src/runtime.ts',
     '^messageformat-runtime/lib/(.*)$': '<rootDir>/packages/runtime/src/$1.ts'
   },
