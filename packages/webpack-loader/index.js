f3b9d6172e3aef5f559bdb8718c89f68c93c3344
Eemeli Aro <eemeli@gmail.com>
2020-11-16 16:23:07 +0200

    feat(loader): Add support for .properties files + new options

--- packages/webpack-loader/index.js
+++ packages/webpack-loader/index.js
@@ -2,7 +2,9 @@ const loaderUtils = require('loader-utils');
 const MessageFormat = require('@messageformat/core');
 const compileModule = require('@messageformat/core/compile-module');
 const convertToMessageFormat = require('@messageformat/convert');
+const { parse } = require('dot-properties');
 const { relative } = require('path');
+const uv = require('uv');
 const YAML = require('yaml');
 
 function localeFromResourcePath(resourcePath, locales) {
@@ -20,8 +22,16 @@ function localeFromResourcePath(resourcePath, locales) {
 }
 
 module.exports = function loadMessages(content) {
-  let { convert, locale, ...mfOpt } = loaderUtils.getOptions(this) || {};
-  let messages = YAML.parse(content);
+  let { convert, encoding, locale, propKeyPath = true, ...mfOpt } =
+    loaderUtils.getOptions(this) || {};
+
+  if (!encoding || encoding === 'auto')
+    encoding = uv(content) ? 'utf8' : 'latin1';
+  const input = content.toString(encoding);
+
+  let messages = this.resourcePath.endsWith('.properties')
+    ? parse(input, propKeyPath)
+    : YAML.parse(input);
 
   if (convert) {
     const cm = convertToMessageFormat(messages, convert);
@@ -39,3 +49,6 @@ module.exports = function loadMessages(content) {
   const messageFormat = new MessageFormat(locale, mfOpt);
   return compileModule(messageFormat, messages);
 };
+
+// get content as Buffer rather than string
+module.exports.raw = true;
