c38d1151cdfdbc3d655f50f3e1da6db57987a9a1
Eemeli Aro <eemeli@gmail.com>
2021-04-04 18:16:20 +0300

    chore: Drop unused Babel plugins

--- babel.config.js
+++ babel.config.js
@@ -3,6 +3,5 @@ module.exports = {
     ['@babel/preset-env', { targets: { node: 'current' } }],
     '@babel/preset-react',
     '@babel/preset-typescript'
-  ],
-  plugins: ['@babel/plugin-proposal-class-properties']
+  ]
 };
