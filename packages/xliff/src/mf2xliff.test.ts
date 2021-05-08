883a4e73303827304623bd6d6cf93d9450059fdf
Eemeli Aro <eemeli@gmail.com>
2021-05-08 15:48:50 +0300

    feat: Combine source & target in mf2xliff(); add tests

---
+++ packages/xliff/src/mf2xliff.test.ts
@@ -0,0 +1,201 @@
+// @ts-ignore
+import { source } from 'common-tags';
+import { compileFluent } from '@messageformat/compiler';
+import { mf2xliff, stringify } from './index';
+
+test('source only', () => {
+  const src = source`
+    msg = Message
+    var = Foo { $num }
+    select = {$selector ->
+        [a] A
+       *[b] B
+    }`;
+  const res = compileFluent(src, { id: 'res', locale: 'en' });
+  const xliff = stringify(mf2xliff(res));
+  expect(xliff).toBe(
+    source`
+    <xliff version="2.0" srcLang="en" xmlns="urn:oasis:names:tc:xliff:document:2.0" xmlns:mf="http://www.unicode.org/ns/2021/messageformat/2.0/not-real-yet">
+      <file id="f:res" mf:resourceId="res">
+        <unit id="u:msg" name="msg">
+          <segment>
+            <source>Message</source>
+          </segment>
+        </unit>
+        <unit id="u:var" name="var">
+          <mf:messageformat>
+            <mf:variable id="m1">
+              <mf:literal>num</mf:literal>
+            </mf:variable>
+          </mf:messageformat>
+          <segment>
+            <source>Foo·
+              <ph id="1" mf:ref="m1"/>
+            </source>
+          </segment>
+        </unit>
+        <group id="g:select" name="select" mf:select="m2">
+          <mf:messageformat>
+            <mf:variable default="b" id="m2">
+              <mf:literal>selector</mf:literal>
+            </mf:variable>
+          </mf:messageformat>
+          <unit id="u:select.a" name="a">
+            <segment>
+              <source>A</source>
+            </segment>
+          </unit>
+          <unit id="u:select.b" name="b">
+            <segment>
+              <source>B</source>
+            </segment>
+          </unit>
+        </group>
+      </file>
+    </xliff>`.replace(/·/g, ' ')
+  );
+});
+
+test('combine source & target', () => {
+  const src = source`
+    msg = Message
+    var = Foo { $num }
+    select = {$selector ->
+        [a] A
+       *[b] B
+    }`;
+  const srcRes = compileFluent(src, { id: 'res', locale: 'en' });
+
+  const trg = source`
+    msg = Viesti
+    var = Föö { $num }
+    select = {$selector ->
+        [a] Ä
+       *[b] B
+    }`;
+  const trgRes = compileFluent(trg, { id: 'res', locale: 'fi' });
+
+  const xliff = stringify(mf2xliff(srcRes, trgRes));
+  expect(xliff).toBe(
+    source`
+    <xliff version="2.0" srcLang="en" xmlns="urn:oasis:names:tc:xliff:document:2.0" xmlns:mf="http://www.unicode.org/ns/2021/messageformat/2.0/not-real-yet" trgLang="fi">
+      <file id="f:res" mf:resourceId="res">
+        <unit id="u:msg" name="msg">
+          <segment>
+            <source>Message</source>
+            <target>Viesti</target>
+          </segment>
+        </unit>
+        <unit id="u:var" name="var">
+          <mf:messageformat>
+            <mf:variable id="m1">
+              <mf:literal>num</mf:literal>
+            </mf:variable>
+            <mf:variable id="m2">
+              <mf:literal>num</mf:literal>
+            </mf:variable>
+          </mf:messageformat>
+          <segment>
+            <source>Foo·
+              <ph id="1" mf:ref="m1"/>
+            </source>
+            <target>Föö·
+              <ph id="2" mf:ref="m2"/>
+            </target>
+          </segment>
+        </unit>
+        <group id="g:select" name="select" mf:select="m3">
+          <mf:messageformat>
+            <mf:variable default="b" id="m3">
+              <mf:literal>selector</mf:literal>
+            </mf:variable>
+          </mf:messageformat>
+          <unit id="u:select.a" name="a">
+            <segment>
+              <source>A</source>
+              <target>Ä</target>
+            </segment>
+          </unit>
+          <unit id="u:select.b" name="b">
+            <segment>
+              <source>B</source>
+              <target>B</target>
+            </segment>
+          </unit>
+        </group>
+      </file>
+    </xliff>`.replace(/·/g, ' ')
+  );
+});
+
+test('selector mismatch between source & target languages', () => {
+  const src = source`
+    select = {$gender ->
+        [masculine] his house
+        [feminine] her house
+       *[other] their house
+    }`;
+  const srcRes = compileFluent(src, { id: 'res', locale: 'en' });
+
+  const trg = source`
+    select = {$case ->
+        [allative] hänen talolle
+       *[nominative] hänen talo
+    }`;
+  const trgRes = compileFluent(trg, { id: 'res', locale: 'fi' });
+
+  const xliff = stringify(mf2xliff(srcRes, trgRes));
+  expect(xliff).toBe(
+    source`
+    <xliff version="2.0" srcLang="en" xmlns="urn:oasis:names:tc:xliff:document:2.0" xmlns:mf="http://www.unicode.org/ns/2021/messageformat/2.0/not-real-yet" trgLang="fi">
+      <file id="f:res" mf:resourceId="res">
+        <group id="g:select" name="select" mf:select="m1 m2">
+          <mf:messageformat>
+            <mf:variable default="other" id="m1">
+              <mf:literal>gender</mf:literal>
+            </mf:variable>
+            <mf:variable default="nominative" id="m2">
+              <mf:literal>case</mf:literal>
+            </mf:variable>
+          </mf:messageformat>
+          <unit id="u:select.masculine_allative" name="masculine allative">
+            <segment>
+              <source>his house</source>
+              <target>hänen talolle</target>
+            </segment>
+          </unit>
+          <unit id="u:select.masculine_nominative" name="masculine nominative">
+            <segment>
+              <source>his house</source>
+              <target>hänen talo</target>
+            </segment>
+          </unit>
+          <unit id="u:select.feminine_allative" name="feminine allative">
+            <segment>
+              <source>her house</source>
+              <target>hänen talolle</target>
+            </segment>
+          </unit>
+          <unit id="u:select.feminine_nominative" name="feminine nominative">
+            <segment>
+              <source>her house</source>
+              <target>hänen talo</target>
+            </segment>
+          </unit>
+          <unit id="u:select.other_allative" name="other allative">
+            <segment>
+              <source>their house</source>
+              <target>hänen talolle</target>
+            </segment>
+          </unit>
+          <unit id="u:select.other_nominative" name="other nominative">
+            <segment>
+              <source>their house</source>
+              <target>hänen talo</target>
+            </segment>
+          </unit>
+        </group>
+      </file>
+    </xliff>`.replace(/·/g, ' ')
+  );
+});
--- packages/xliff/src/mf2xliff.ts
+++ packages/xliff/src/mf2xliff.ts
@@ -1,3 +1,4 @@
+import deepEqual from 'fast-deep-equal';
 import {
   isFunctionReference,
   isLiteral,
@@ -13,39 +14,50 @@ import type * as X from './xliff-spec';
 let _id = 0;
 const nextId = () => `m${++_id}`;
 
-export function mf2xliff(mf: MF.MessageFormat | MF.Resource): X.Xliff {
+export function mf2xliff(
+  source: MF.MessageFormat | MF.Resource,
+  target?: MF.MessageFormat | MF.Resource
+): X.Xliff {
   _id = 0;
-  let srcLang: string;
-  let elements: X.File[];
-  if (mf instanceof MessageFormat) {
-    srcLang = mf.locales[0];
-    elements = mf.resources.map(handleResource);
-  } else {
-    srcLang = mf.locale;
-    elements = [handleResource(mf)];
-  }
-  return {
-    type: 'element',
-    name: 'xliff',
-    attributes: {
-      version: '2.0',
-      srcLang,
-      xmlns: 'urn:oasis:names:tc:xliff:document:2.0',
-      'xmlns:mf':
-        'http://www.unicode.org/ns/2021/messageformat/2.0/not-real-yet' // FIXME
-    },
-    elements
+  const attributes: X.Xliff['attributes'] = {
+    version: '2.0',
+    srcLang: '',
+    xmlns: 'urn:oasis:names:tc:xliff:document:2.0',
+    'xmlns:mf': 'http://www.unicode.org/ns/2021/messageformat/2.0/not-real-yet' // FIXME
   };
+  const elements: X.File[] = [];
+  if (source instanceof MessageFormat) {
+    attributes.srcLang = source.locales[0];
+    if (target instanceof MessageFormat) attributes.trgLang = target.locales[0];
+    else if (target)
+      throw new Error('source and target must be of the same type');
+    for (const sr of source.resources) {
+      const tr = target?.resources.find(res => res.id === sr.id);
+      elements.push(resolveResource(sr, tr));
+    }
+  } else {
+    attributes.srcLang = source.locale;
+    if (target instanceof MessageFormat)
+      throw new Error('source and target must be of the same type');
+    else if (target) attributes.trgLang = target.locale;
+    elements.push(resolveResource(source, target));
+  }
+  return { type: 'element', name: 'xliff', attributes, elements };
 }
 
-const handleResource = (res: MF.Resource): X.File => ({
-  type: 'element',
-  name: 'file',
-  attributes: { id: `f:${res.id}` },
-  elements: Object.entries(res.entries).map(([key, msg]) =>
-    handleEntry([key], msg)
-  )
-});
+function resolveResource(
+  source: MF.Resource,
+  target: MF.Resource | undefined
+): X.File {
+  return {
+    type: 'element',
+    name: 'file',
+    attributes: { id: `f:${source.id}`, 'mf:resourceId': source.id },
+    elements: Object.entries(source.entries).map(([key, srcMsg]) =>
+      resolveEntry([key], srcMsg, target?.entries[key])
+    )
+  };
+}
 
 const msgAttributes = (pre: 'g' | 'u', key: string[]) => ({
   id: `${pre}:${key.join('.').replace(/ +/g, '_')}`,
@@ -58,83 +70,206 @@ const asText = (value: unknown): X.Text => ({
   text: String(value)
 });
 
-function handleEntry(
-  key: string[],
-  msg: MF.Message | MF.MessageGroup
-): X.Unit | X.Group {
-  if (isMessage(msg))
-    return isSelect(msg.value)
-      ? handleSelect(key, msg.value)
-      : handlePattern(key, msg.value);
+const mismatch = (key: string[]) =>
+  `Structure mismatch between source & target at ${key.join('.')}`;
 
+function resolveEntry(
+  key: string[],
+  srcMsg: MF.Message | MF.MessageGroup,
+  trgMsg: MF.Message | MF.MessageGroup | undefined
+): X.Unit | X.Group {
+  if (isMessage(srcMsg)) {
+    if (trgMsg) {
+      if (!isMessage(trgMsg)) throw new Error(mismatch(key));
+      if (isSelect(srcMsg.value) || isSelect(trgMsg.value))
+        return resolveSelect(key, srcMsg.value, trgMsg.value);
+      else return resolvePattern(key, srcMsg.value, trgMsg.value);
+    } else {
+      return isSelect(srcMsg.value)
+        ? resolveSelect(key, srcMsg.value, undefined)
+        : resolvePattern(key, srcMsg.value, undefined);
+    }
+  }
+
+  if (trgMsg && isMessage(trgMsg)) throw new Error(mismatch(key));
   return {
     type: 'element',
     name: 'group',
     attributes: msgAttributes('g', key),
-    elements: Object.entries(msg.entries).map(([k, msg]) =>
-      handleEntry([...key, k], msg)
+    elements: Object.entries(srcMsg.entries).map(([k, srcMsg]) =>
+      resolveEntry([...key, k], srcMsg, trgMsg?.entries[k])
     )
   };
 }
 
-function handleSelect(key: string[], sel: MF.Select): X.Group {
-  const selSrc: string[] = [];
-  const parts: X.MessagePart[] = sel.select.map(p => {
+function resolveSelect(
+  key: string[],
+  srcSel: MF.Part[] | MF.Select,
+  trgSel: MF.Part[] | MF.Select | undefined
+): X.Group {
+  // We might be combining a Pattern and a Select, so let's normalise
+  if (Array.isArray(srcSel)) {
+    if (!trgSel || Array.isArray(trgSel))
+      throw new Error(
+        `At least one of source & target at ${key.join('.')} must be a select`
+      );
+    srcSel = { select: trgSel.select, cases: [{ key: [], value: srcSel }] };
+  } else if (Array.isArray(trgSel))
+    trgSel = { select: srcSel.select, cases: [{ key: [], value: trgSel }] };
+
+  const select: {
+    id: string;
+    default: string | number;
+    keys: (string | number)[];
+  }[] = [];
+  const parts: X.MessagePart[] = srcSel.select.map(sel => {
     const id = nextId();
-    selSrc.push(id);
-    const part = handlePart(id, p.value);
-    if (isLiteral(p.default))
-      part.attributes = Object.assign({ default: p.default }, part.attributes);
-    return part;
+    select.push({ id, default: sel.default ?? 'other', keys: [] });
+    return resolveSelector(id, sel);
   });
 
   const elements: (X.MessageFormat | X.Unit)[] = [
     { type: 'element', name: 'mf:messageformat', elements: parts }
   ];
-  for (const c of sel.cases)
-    elements.push(handlePattern([...key, c.key.join(' ')], c.value));
+
+  if (!trgSel) {
+    // If there's only a source, we use its cases directly
+    for (const c of srcSel.cases)
+      elements.push(
+        resolvePattern([...key, c.key.join(' ')], c.value, undefined)
+      );
+  } else {
+    // If the source and target have different selectors, it gets a bit complicated.
+    // First, let's make sure that `selIds` and `parts` includes all the selectors
+    // and that we have mappings between the array indices.
+    const trgSelMap: number[] = [];
+    for (const sel of trgSel.select) {
+      const prevIdx = srcSel.select.findIndex(prev => deepEqual(sel, prev));
+      if (prevIdx !== -1) trgSelMap.push(prevIdx);
+      else {
+        const id = nextId();
+        select.push({ id, default: sel.default ?? 'other', keys: [] });
+        trgSelMap.push(select.length - 1);
+        parts.push(resolveSelector(id, sel));
+      }
+    }
+
+    // Collect all of the key values for each case, in the right order
+    const addSorted = (i: number, key: string | number) => {
+      const { default: def, keys } = select[i];
+      if (keys.includes(key)) return;
+      if (key === def) keys.push(key);
+      else if (typeof key === 'number') {
+        let pos = 0;
+        while (keys[pos] !== def && typeof keys[pos] === 'number') pos += 1;
+        keys.splice(pos, 0, key);
+      } else {
+        let pos = keys.length;
+        while (keys[pos - 1] === def) pos -= 1;
+        keys.splice(pos, 0, key);
+      }
+    };
+    for (const c of srcSel.cases)
+      for (let i = 0; i < c.key.length; ++i) addSorted(i, c.key[i]);
+    for (const c of trgSel.cases)
+      for (let i = 0; i < c.key.length; ++i) addSorted(trgSelMap[i], c.key[i]);
+
+    // Add a separate entry for each combined case
+    // TODO: Collapse duplicates to default value only, where possible
+    for (const sk of everyKey(select)) {
+      const srcCase = srcSel.cases.find(c =>
+        c.key.every((k, i) => k === sk[i] || k === select[i].default)
+      );
+      const trgCase = trgSel.cases.find(c =>
+        c.key.every((k, i) => {
+          const ti = trgSelMap[i];
+          return k === sk[ti] || k === select[ti].default;
+        })
+      );
+      if (!srcCase || !trgCase)
+        throw new Error(`Case ${sk} not found‽ src:${srcCase} trg:${trgCase}`);
+      elements.push(
+        resolvePattern([...key, sk.join(' ')], srcCase.value, trgCase.value)
+      );
+    }
+  }
 
   return {
     type: 'element',
     name: 'group',
     attributes: Object.assign(msgAttributes('g', key), {
-      'mf:select': selSrc.join(' ')
+      'mf:select': select.map(s => s.id).join(' ')
     }),
     elements
   };
 }
 
-function handlePattern(key: string[], pattern: MF.Part[]): X.Unit {
-  const parts: X.MessagePart[] = [];
-  const source: X.Source = { type: 'element', name: 'source', elements: [] };
-  for (const p of pattern) {
-    if (isLiteral(p)) source.elements.push(asText(p));
-    else {
-      const id = nextId();
-      const part = handlePart(id, p);
-      parts.push(part);
-      const attributes = { id: id.substring(1), 'mf:ref': id };
-      source.elements.push({ type: 'element', name: 'ph', attributes });
-    }
-  }
-  const elements: (X.MessageFormat | X.Segment)[] = [
-    { type: 'element', name: 'segment', elements: [source] }
-  ];
-  if (parts.length > 0)
-    elements.unshift({
-      type: 'element',
-      name: 'mf:messageformat',
-      elements: parts
-    });
-  return {
-    type: 'element',
-    name: 'unit',
-    attributes: msgAttributes('u', key),
-    elements
-  };
+function resolveSelector(id: string, sel: MF.Selector) {
+  const part = resolvePart(id, sel.value);
+  if (isLiteral(sel.default))
+    part.attributes = Object.assign({ default: sel.default }, part.attributes);
+  return part;
 }
 
-function handlePart(id: string | null, part: MF.Part | boolean): X.MessagePart {
+function everyKey(select: { keys: MF.Literal[] }[]): Iterable<MF.Literal[]> {
+  let ptr: number[] | null = null;
+  const max = select.map(s => s.keys.length - 1);
+  function next(): IteratorResult<MF.Literal[]> {
+    if (!ptr) ptr = new Array<number>(select.length).fill(0);
+    else {
+      for (let i = ptr.length - 1; i >= 0; --i) {
+        if (ptr[i] < max[i]) {
+          ptr[i] += 1;
+          break;
+        }
+        if (i === 0) return { done: true, value: undefined };
+        ptr[i] = 0;
+      }
+    }
+    return { value: ptr.map((j, i) => select[i].keys[j]) };
+  }
+  return { [Symbol.iterator]: () => ({ next }) };
+}
+
+function resolvePattern(
+  key: string[],
+  srcPattern: MF.Part[],
+  trgPattern: MF.Part[] | undefined
+): X.Unit {
+  const parts: X.MessagePart[] = [];
+  const handlePart = (p: MF.Part): X.Text | X.InlineElement => {
+    if (isLiteral(p)) return asText(p);
+    const id = nextId();
+    const part = resolvePart(id, p);
+    parts.push(part);
+    const attributes = { id: id.substring(1), 'mf:ref': id };
+    return { type: 'element', name: 'ph', attributes };
+  };
+
+  const se = srcPattern.map(handlePart);
+  const source: X.Source = { type: 'element', name: 'source', elements: se };
+  let ge: X.Segment['elements'];
+  if (trgPattern) {
+    const te = trgPattern.map(handlePart);
+    const target: X.Target = { type: 'element', name: 'target', elements: te };
+    ge = [source, target];
+  } else ge = [source];
+  const segment: X.Segment = { type: 'element', name: 'segment', elements: ge };
+
+  const attributes = msgAttributes('u', key);
+  let elements: X.Unit['elements'];
+  if (parts.length > 0) {
+    const name = 'mf:messageformat';
+    const mf: X.MessageFormat = { type: 'element', name, elements: parts };
+    elements = [mf, segment];
+  } else elements = [segment];
+  return { type: 'element', name: 'unit', attributes, elements };
+}
+
+function resolvePart(
+  id: string | null,
+  part: MF.Part | boolean
+): X.MessagePart {
   const attributes = id ? { id } : undefined;
 
   if (isLiteral(part) || typeof part === 'boolean') {
@@ -147,7 +282,7 @@ function handlePart(id: string | null, part: MF.Part | boolean): X.MessagePart {
   }
 
   if (isVariableReference(part)) {
-    const elements = part.var_path.map(p => handlePart(null, p));
+    const elements = part.var_path.map(p => resolvePart(null, p));
     return { type: 'element', name: 'mf:variable', attributes, elements };
   }
 
@@ -161,7 +296,7 @@ function handlePart(id: string | null, part: MF.Part | boolean): X.MessagePart {
           attributes: { name },
           elements: [asText(value)]
         });
-    for (const p of part.args) elements.push(handlePart(null, p));
+    for (const p of part.args) elements.push(resolvePart(null, p));
     return {
       type: 'element',
       name: 'mf:function',
@@ -179,10 +314,10 @@ function handlePart(id: string | null, part: MF.Part | boolean): X.MessagePart {
           name: 'mf:scope',
           attributes: { name },
           elements: Array.isArray(value)
-            ? value.map(v => handlePart(null, v))
-            : [handlePart(null, value)]
+            ? value.map(v => resolvePart(null, v))
+            : [resolvePart(null, value)]
         });
-    for (const p of part.msg_path) elements.push(handlePart(null, p));
+    for (const p of part.msg_path) elements.push(resolvePart(null, p));
     return {
       type: 'element',
       name: 'mf:message',
