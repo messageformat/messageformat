f04ecfd28d4506297ae8fe2ef4e2931827fd1528
Eemeli Aro <eemeli@gmail.com>
2021-08-22 20:12:52 +0300

    refactor: Inline MF1 implementations, dropping @messageformat/runtime as dependency

--- packages/messageformat/src/runtime/mf1.ts
+++ packages/messageformat/src/runtime/mf1.ts
@@ -1,51 +1,119 @@
-import {
-  date as dateFmt,
-  duration as durationFmt,
-  numberFmt,
-  time as timeFmt
-} from '@messageformat/runtime/lib/formatters';
+import { FormattableDateTime, FormattableNumber } from '../formattable';
 import { runtime as MF2 } from './default';
 import type { RuntimeFunction, RuntimeOptions } from './index';
-
-const asLiteral = (arg: unknown) =>
-  typeof arg === 'number' || typeof arg === 'string' ? arg : String(arg);
 
 const getParam = (options: RuntimeOptions | undefined) =>
   (options && String(options.param).trim()) || undefined;
 
 type DateTimeSize = 'short' | 'default' | 'long' | 'full';
 
-export const date: RuntimeFunction<string> = {
+export const date: RuntimeFunction<FormattableDateTime> = {
   call: function date(
     locales: string[],
     options: RuntimeOptions | undefined,
     arg: unknown
   ) {
-    return dateFmt(asLiteral(arg), locales, getParam(options) as DateTimeSize);
+    const date =
+      arg instanceof Date || arg instanceof FormattableDateTime
+        ? arg
+        : new Date(typeof arg === 'number' ? arg : String(arg));
+    const size = getParam(options) as DateTimeSize;
+    const opt: Intl.DateTimeFormatOptions = {
+      localeMatcher: options?.localeMatcher,
+      weekday: size === 'full' ? 'long' : undefined,
+      day: 'numeric',
+      month:
+        size === 'short'
+          ? 'numeric'
+          : size === 'full' || size === 'long'
+          ? 'long'
+          : 'short',
+      year: 'numeric'
+    };
+    return new FormattableDateTime(date, locales, opt);
   },
 
+  formattable: FormattableDateTime,
   options: { param: 'string' }
 };
 
+/**
+ * Represent a duration in seconds as a string
+ *
+ * @return Includes one or two `:` separators, and matches the pattern
+ *   `hhhh:mm:ss`, possibly with a leading `-` for negative values and a
+ *   trailing `.sss` part for non-integer input
+ */
 export const duration: RuntimeFunction<string> = {
   call: function duration(_locales: string[], _options: unknown, arg: unknown) {
-    return durationFmt(asLiteral(arg));
+    let value = typeof arg === 'number' ? arg : Number(arg);
+    if (!isFinite(value)) return String(value);
+    let sign = '';
+    if (value < 0) {
+      sign = '-';
+      value = Math.abs(value);
+    } else {
+      value = Number(value);
+    }
+    const sec = value % 60;
+    const parts = [Math.round(sec) === sec ? sec : sec.toFixed(3)];
+    if (value < 60) {
+      parts.unshift(0); // at least one : is required
+    } else {
+      value = Math.round((value - Number(parts[0])) / 60);
+      parts.unshift(value % 60); // minutes
+      if (value >= 60) {
+        value = Math.round((value - Number(parts[0])) / 60);
+        parts.unshift(value); // hours
+      }
+    }
+    const first = parts.shift();
+    return (
+      sign +
+      first +
+      ':' +
+      parts.map(n => (n < 10 ? '0' + String(n) : String(n))).join(':')
+    );
   },
   options: 'never'
 };
 
-export const number: RuntimeFunction<string> = {
+export const number: RuntimeFunction<FormattableNumber> = {
   call: function number(
     locales: string[],
     options: RuntimeOptions | undefined,
     arg: unknown
   ) {
-    let n = Number(arg);
-    const offset = Number(options && options.pluralOffset);
-    if (Number.isFinite(offset)) n -= offset;
-    return numberFmt(n, locales, getParam(options) || '', 'USD');
+    let num =
+      typeof arg === 'number' ||
+      typeof arg === 'bigint' ||
+      arg instanceof FormattableNumber
+        ? arg
+        : Number(arg);
+    const offset = Number(options?.pluralOffset);
+    if (Number.isFinite(offset)) {
+      if (num instanceof FormattableNumber) num = num.value; // FIXME
+      if (typeof num === 'bigint') num -= BigInt(offset);
+      else num -= offset;
+    }
+    const param = getParam(options);
+    let opt: Intl.NumberFormatOptions | undefined = undefined;
+    switch (param) {
+      case 'integer':
+        opt = { maximumFractionDigits: 0 };
+        break;
+      case 'percent':
+        opt = { style: 'percent' };
+        break;
+      case 'currency': {
+        opt = { style: 'currency', currency: 'USD' };
+        break;
+      }
+    }
+    return new FormattableNumber(num, locales, opt);
   },
 
+  formattable: FormattableNumber,
   options: {
     param: 'string',
     pluralOffset: 'number'
@@ -69,15 +137,28 @@ export const plural: RuntimeFunction<string[]> = {
   options: Object.assign({ pluralOffset: 'number' }, MF2.plural.options)
 };
 
-export const time: RuntimeFunction<string> = {
+export const time: RuntimeFunction<FormattableDateTime> = {
   call: function time(
     locales: string[],
     options: RuntimeOptions | undefined,
     arg: unknown
   ) {
-    return timeFmt(asLiteral(arg), locales, getParam(options) as DateTimeSize);
+    const time =
+      arg instanceof Date || arg instanceof FormattableDateTime
+        ? arg
+        : new Date(typeof arg === 'number' ? arg : String(arg));
+    const size = getParam(options) as DateTimeSize;
+    const opt: Intl.DateTimeFormatOptions = {
+      localeMatcher: options?.localeMatcher,
+      second: size === 'short' ? undefined : 'numeric',
+      minute: 'numeric',
+      hour: 'numeric',
+      timeZoneName: size === 'full' || size === 'long' ? 'short' : undefined
+    };
+    return new FormattableDateTime(time, locales, opt);
   },
 
+  formattable: FormattableDateTime,
   options: { param: ['short', 'default', 'long', 'full'] }
 };
 
