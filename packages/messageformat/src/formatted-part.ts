728bb268ec117df2be1aac9cf669c12826b30585
Eemeli Aro <eemeli@gmail.com>
2022-02-05 12:17:46 +0200

    chore: Drop unused formatted-part.ts

--- packages/messageformat/src/formatted-part.ts
+++
@@ -1,9 +0,0 @@
-import type { Meta } from './data-model';
-
-export type MessageFormatPart =
-  | (Intl.DateTimeFormatPart & { source?: string })
-  | (Intl.NumberFormatPart & { source?: string })
-  | { type: 'literal'; value: string; source?: string }
-  | { type: 'dynamic'; value: unknown; source: string }
-  | { type: 'fallback'; value: string; source: string }
-  | { type: 'meta'; value: ''; meta: Meta; source?: string };
--- packages/messageformat/src/index.ts
+++ packages/messageformat/src/index.ts
@@ -1,6 +1,5 @@
 export * from './data-model';
 export * from './message-value';
-export type { MessageFormatPart } from './formatted-part';
 export { MessageFormat } from './messageformat';
 export * from './pattern';
 export { ResourceReader } from './resource-reader';
