7904d7f1d21b6b8ef67bc3ed2e21f55e7e4de175
Eemeli Aro <eemeli@gmail.com>
2022-07-31 20:04:20 +0200

    feat(mf2-icu-mf1)!: Drop locale-specific CLDR category checks from mf1ToMessageData()
    This also allows dropping make-plural as a dependency.

--- packages/mf2-icu-mf1/src/compile.ts
+++ packages/mf2-icu-mf1/src/compile.ts
@@ -1,23 +1,7 @@
 import { parse } from '@messageformat/parser';
-import * as PluralCategories from 'make-plural/pluralCategories';
 import { Message, MessageFormat, MessageFormatOptions } from 'messageformat';
 import { astToMessage } from './ast-to-message';
 import { getMF1Runtime } from './runtime';
-
-const isPluralId = (id: string): id is keyof typeof PluralCategories =>
-  id in PluralCategories;
-
-function normalize(locale: string) {
-  if (typeof locale !== 'string' || locale.length < 2)
-    throw new Error(`Invalid language tag: ${locale}`);
-
-  // The only locale for which anything but the primary subtag matters is
-  // Portuguese as spoken in Portugal.
-  if (locale.startsWith('pt-PT')) return 'pt_PT';
-
-  const m = locale.match(/.+?(?=[-_])/);
-  return m ? m[0] : locale;
-}
 
 /** @beta */
 export type MF1Options = {
@@ -33,20 +17,18 @@ export type MF1Options = {
  * @beta
  * @param source - An ICU MessageFormat message, either as its string contents,
  *   or as a {@link messageformat#Message} data structure.
- * @param locale - The locale code to use for the message.
+ * @param locales - The locale to use for the message.
  * @param options - See {@link MF1Options} and {@link messageformat#MessageFormatOptions}
  */
 export function mf1ToMessage(
   source: string | Message,
-  locale: string,
+  locales?: string | string[],
   { strict, ...opt }: MF1Options & MessageFormatOptions = {}
 ): MessageFormat {
   const msg =
-    typeof source === 'string'
-      ? mf1ToMessageData(source, locale, { strict })
-      : source;
+    typeof source === 'string' ? mf1ToMessageData(source, { strict }) : source;
   opt.runtime = Object.assign(getMF1Runtime(), opt.runtime);
-  return new MessageFormat(msg, [locale], opt);
+  return new MessageFormat(msg, locales, opt);
 }
 
 /**
@@ -54,17 +36,12 @@ export function mf1ToMessage(
  *
  * @beta
  * @param src - A Fluent resource, as the string contents of an FTL file.
- * @param locale - The locale code to use for the message.
  * @param options - See {@link MF1Options}
  */
 export function mf1ToMessageData(
   src: string,
-  locale: string,
   { strict }: MF1Options = {}
 ): Message {
-  const lc = normalize(locale);
-  if (!isPluralId(lc)) throw new Error(`Unsupported locale: ${locale}`);
-  const { cardinal, ordinal } = PluralCategories[lc];
-  const ast = parse(src, { cardinal, ordinal, strict });
+  const ast = parse(src, { strict });
   return astToMessage(ast);
 }
