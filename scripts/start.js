34d165dbcaf290f7916e115552a6f71ee2d0228c
Eemeli Aro <eemeli@gmail.com>
2023-03-11 13:17:31 +0200

    chore: Add compileModule to `npm start` playground script

--- scripts/start.js
+++ scripts/start.js
@@ -1,5 +1,6 @@
 global.convert = require('../packages/convert');
 global.MessageFormat = require('../packages/core');
+global.compileModule = require('../packages/core/compile-module');
 global.parse = require('../packages/parser').parse;
 
 global.mf = new global.MessageFormat('en');
