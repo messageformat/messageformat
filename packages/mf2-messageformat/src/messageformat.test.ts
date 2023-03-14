5422cd3e54e6031ce248d669d624006e3905e6a9
Eemeli Aro <eemeli@gmail.com>
2023-03-14 13:27:56 +0200

    test(mf2): Add "only" filtering to test-messages.json tests

--- packages/mf2-messageformat/src/messageformat.test.ts
+++ packages/mf2-messageformat/src/messageformat.test.ts
@@ -4,8 +4,8 @@ import { testName } from './__fixtures/test-name';
 
 describe('Format messages', () => {
   for (const testMsg of testMessages) {
-    test(testName(testMsg), () => {
-      const { src, exp, locale, params, errors: expErrors } = testMsg;
+    const { src, exp, locale, params, errors: expErrors, only } = testMsg;
+    (only ? test.only : test)(testName(testMsg), () => {
       const errors: any[] = [];
       const mf = new MessageFormat(src, locale ?? 'en');
       const msg = mf.resolveMessage(params, err => errors.push(err));
