9f5e0bfbbe4731ebbe1f0b87f06af743e6e4bc0d
Eemeli Aro <eemeli@gmail.com>
2025-03-22 11:54:34 +0200

    docs(mf2): Update API docs, add Fluent MessageReferencePart interface

--- mf2/messageformat/src/message-value.ts
+++ mf2/messageformat/src/message-value.ts
@@ -4,6 +4,9 @@ export const BIDI_ISOLATE = Symbol('bidi-isolate');
 
 /**
  * The base for all resolved message values.
+ *
+ * @typeParam T - The uniquely identifying `type` of the message value.
+ * @typeParam P - The `"type"` used in the formatted-parts representation of the message value.
  */
 export interface MessageValue<T extends string, P extends string = T> {
   readonly type: T;
