03bef300b0a06e9279ed33a862c79f6a3d89d5ee
Eemeli Aro <eemeli@gmail.com>
2025-03-22 12:20:59 +0200

    feat(mf2/icu-mf1)!: Refactor getMF1Functions() as MF1Functions

--- mf2/icu-messageformat-1/src/mf1-to-message.ts
+++ mf2/icu-messageformat-1/src/mf1-to-message.ts
@@ -4,7 +4,7 @@ import {
   MessageFormat,
   MessageFormatOptions
 } from 'messageformat';
-import { getMF1Functions } from './functions.ts';
+import { MF1Functions } from './functions.ts';
 import { mf1ToMessageData } from './mf1-to-message-data.ts';
 
 export type MF1Options = {
@@ -14,8 +14,6 @@ export type MF1Options = {
 
 /**
  * Compile an ICU MessageFormat 1 message into a {@link MessageFormat} instance.
- *
- * A runtime provided by {@link getMF1Functions} is automatically used in these instances.
  *
  * @param source - An ICU MessageFormat message, either in its syntax representation,
  *   as an array of `@messageformat/parser` {@link https://messageformat.github.io/messageformat/api/parser.parse/ | AST tokens},
@@ -37,6 +35,8 @@ export function mf1ToMessage(
   } else {
     msg = source;
   }
-  opt.functions = Object.assign(getMF1Functions(), opt.functions);
+  opt.functions = opt.functions
+    ? Object.assign(Object.create(null), MF1Functions, opt.functions)
+    : MF1Functions;
   return new MessageFormat(locales, msg, opt);
 }
--- mf2/messageformat/src/mf2-features.test.ts
+++ mf2/messageformat/src/mf2-features.test.ts
@@ -1,6 +1,6 @@
 import { fluentToResource } from '@messageformat/fluent';
 import {
-  getMF1Functions,
+  MF1Functions,
   mf1ToMessage,
   mf1ToMessageData
 } from '@messageformat/icu-messageformat-1';
@@ -136,7 +136,7 @@ describe('Multi-selector messages (unicode-org/message-format-wg#119)', () => {
     expect(msg.selectors).toHaveLength(6);
     expect(msg.variants).toHaveLength(64);
 
-    const mf = new MessageFormat('en', msg, { functions: getMF1Functions() });
+    const mf = new MessageFormat('en', msg, { functions: MF1Functions });
 
     const one = mf.format({
       N: 1,
