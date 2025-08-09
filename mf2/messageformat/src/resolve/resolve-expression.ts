adca3fb245b79bd12fd1fc249846b3c63cba6f8c
Eemeli Aro <eemeli@gmail.com>
2025-08-09 22:45:29 +0300

    refactor(mf2): Inline resolveLiteral() into resolveExpression()

--- mf2/messageformat/src/resolve/resolve-expression.ts
+++ mf2/messageformat/src/resolve/resolve-expression.ts
@@ -1,11 +1,11 @@
 import type { Expression } from '../data-model/types.ts';
 import type { Context } from '../format-context.ts';
 import type { MessageFallback } from '../functions/fallback.ts';
-import type { MessageString } from '../functions/string.ts';
+import { type MessageString, string } from '../functions/string.ts';
 import type { MessageUnknownValue } from '../functions/unknown.ts';
 import type { MessageValue } from '../message-value.ts';
+import { MessageFunctionContext } from './function-context.ts';
 import { resolveFunctionRef } from './resolve-function-ref.ts';
-import { resolveLiteral } from './resolve-literal.ts';
 import { resolveVariableRef } from './resolve-variable.ts';
 
 export function resolveExpression<T extends string, P extends string>(
@@ -20,8 +20,15 @@ export function resolveExpression<T extends string, P extends string>(
     return resolveFunctionRef(ctx, arg, functionRef);
   }
   switch (arg?.type) {
-    case 'literal':
-      return resolveLiteral(ctx, arg);
+    case 'literal': {
+      const source = `|${arg.value}|`;
+      const msgCtx = new MessageFunctionContext(ctx, source);
+      const msgStr = string(msgCtx, {}, arg.value) as MessageString & {
+        source: string;
+      };
+      msgStr.source = source;
+      return msgStr;
+    }
     case 'variable':
       return resolveVariableRef(ctx, arg);
     default:
