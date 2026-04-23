5b40b5ed80c72b13b688b94b627a85753ce9c568
Eemeli Aro <eemeli@gmail.com>
2026-04-23 13:08:44 +0300

    refactor(mf2/xliff): Rename some source files

--- mf2/xliff/src/index.ts
+++ mf2/xliff/src/index.ts
@@ -1,8 +1,8 @@
 import type { Model } from 'messageformat';
 
-export { mf2xliff } from './mf2xliff.ts';
+export { mf2xliff } from './mf2-to-xliff.ts';
 export { parse, stringify } from './xliff.ts';
-export { xliff2mf } from './xliff2mf.ts';
+export { xliff2mf } from './xliff-to-mf2.ts';
 export * from './xliff-spec.ts';
 
 export type MessageResourceData = Map<
